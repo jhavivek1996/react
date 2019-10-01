88e73565e3c385cc59198173adcbc3a1742ee0f4
jhavivek1996 <jhavivek260@gmail.com>
2019-10-01 22:02:58 +0530

    React Commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,32 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+// ReactDOM.render(
+// 	<h1>Hello World</h1>,
+// 	document.getElementById('root1')
+// 	);
+
+function f1(user)
+{
+	return user.firstname + '' + user.lastname;
+}
+
+const user = 
+{
+	firstname : 'Vivek ',
+	lastname : 'Jha',
+};
+
+const element = (
+	<h1> Hello, {f1(user)} </h1>
+	);
+ReactDOM.render(
+	element, document.getElementById('firstdiv'));
